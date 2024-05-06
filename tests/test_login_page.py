0299bf37fd3031b2ecfb96b3c22fecdc6758bc5b
Enrique Sanchez <enrique.sanchez@hpe.com>
2024-05-06 17:26:12 +0200

    feat: move Selenium login tests to pytest class. Register Marks in pytest.ini to avoid warnings in tests execution

---
+++ tests/test_login_page.py
@@ -0,0 +1,56 @@
+# To run script with python using virtual environment we have to:
+# 1. Create the virtual environment with Ctrl+Shift+P look for "python: create virtual environment"
+# 2. Once the Virtual environment is created in .venv folder, choose as python interpreter the python of the virtual environment with
+#    Ctrl+Shift+P look for "python: select interpreter" and choose the virtual environment already created
+
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class TestPositiveScenarios:
+
+    @pytest.mark.login
+    @pytest.mark.positive
+    def test_positive_login(self):
+        # Test steps:
+        # -----------
+        # 1. Open Browser
+        driver = webdriver.Chrome()
+        time.sleep(3)
+
+        # 2. Go to webpage
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        # 3. Fill form inputs with data
+        # To create a locator to our inputs attributes, we have to follow the next rule:
+        # //tag[@attribute='value'], for example: //input[@id='username'] or //button[@class='btn']
+        username_locator = driver.find_element(By.ID, 'username') # get username by the id of the html tag
+        password_locator = driver.find_element(By.NAME, 'password') # get password by the name of the html tag
+        submit_button_locator = driver.find_element(By.XPATH, '//button[@class=\'btn\']') # get the button using the XPATH format
+
+        # type text in form inputs
+        username_locator.send_keys('student')
+        password_locator.send_keys('Password123')
+
+        # 4. Submit the form
+        submit_button_locator.click()
+        time.sleep(2)
+
+        # 5. Validate that the URL where we are redirected is the right one
+        actual_url = driver.current_url
+        assert actual_url.__contains__('practicetestautomation.com/logged-in-successfully/')
+
+        # 6. Validate result page contains success data
+        post_title_locator = driver.find_element(By.TAG_NAME, 'h1') # we can use it here, because we know that the page only has one h1 in the full page
+        post_logout_locator = driver.find_element(By.LINK_TEXT, 'Log out')
+
+        actual_title = post_title_locator.text
+        assert actual_title == 'Logged In Successfully'
+
+        assert post_logout_locator.is_displayed()
+
+        time.sleep(5)
+        print('Atomation tests PASSED!!!')
+
+
--- tests/test_math.py
+++ tests/test_math.py
@@ -1,13 +1,17 @@
 # To install pytest we run in our virtual environment:
 # pip install -U pytest
 
+import pytest
+
 def add_two_numbers(a, b):
     return a + b
 
 # PyTest only execute tests methods that starts by test, and files that starts by test
+@pytest.mark.math
 def test_small_numbers():
     assert add_two_numbers(1, 2) == 3, 'The sum of 1 and 2 should be 3'
 
+@pytest.mark.math
 def test_large_numbers():
     assert add_two_numbers(100, 300) == 400, 'The sum of 100 and 300 should be 400'
 

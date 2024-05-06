b7327b899a17e2b1c1999fa446bda64c121b9841
Enrique Sanchez <enrique.sanchez@hpe.com>
2024-05-06 18:49:42 +0200

    feat: add an additional negative test method. Create requirements file and README with instructions to create requirements.txt file

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to create requirements.txt file
+- Install pipreqs
+    `pip install pipreqs`
+
+- Execute pipreqs
+    - `pipreqs . --ignore ".venv"`
--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -29,3 +29,27 @@ class TestLoginPageNegative:
         error_message_locator = driver.find_element(By.ID, 'error')
         assert error_message_locator.is_displayed(), 'ERROR: is not displayed but it should'
         assert error_message_locator.text == 'Your username is invalid!', 'Error message expected is not present'
+
+    @pytest.mark.login
+    @pytest.mark.negative
+    def test_negative_password(self):
+        # Open page
+        driver = webdriver.Chrome();
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+        
+        # Type username student into username field
+        username_locator = driver.find_element(By.ID, 'username')
+        username_locator.send_keys('student')
+        
+        # Type incorrect password incorrectPassword into password field
+        password_locator = driver.find_element(By.ID, 'password')
+        password_locator.send_keys('incorrectPassword')
+        # Click submit button
+        submit_button_locator = driver.find_element(By.ID, 'submit')
+        submit_button_locator.click()
+        time.sleep(3)
+
+        # Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, 'error')
+        assert error_message_locator.is_displayed(), 'ERROR: is not displayed but it should'
+        assert error_message_locator.text == 'Your password is invalid!', 'Error message expected is not present'

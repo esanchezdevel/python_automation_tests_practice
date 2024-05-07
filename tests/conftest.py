a9b421b7adf15752f5e1d01526299b568065baa6
Enrique Sanchez <enrique.sanchez@hpe.com>
2024-05-07 17:59:53 +0200

    feat: add pytest addoption to accept the browser parameter in the pytest request to choose the browser to be executed

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,21 @@ from selenium import webdriver
 
 # Fixture to be executed before and after the tests execution
 @pytest.fixture
-def driver():
-    print('Creating Chrome driver')
-    #mydriver = webdriver.Chrome()
-    mydriver = webdriver.Firefox()
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f'Creating {browser} browser')
+    if browser == 'chrome':
+        mydriver = webdriver.Chrome()
+    elif browser == 'firefox':
+        mydriver = webdriver.Firefox()
+    else:
+        raise TypeError(f'Expected \'chrome\' or \'firefox\' but got \'{browser}\'')
     # Return the mydriver variable using the keyword yield. It's the same as return
     # But in this case all the things before the yield keywords are executed BEFORE the tests execution
     yield mydriver
     # And the things that are after the yield keyword, are executed AFTER the tests execution
     print('Closing Chrome driver')
     mydriver.quit()
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='chrome', help='browser to execute tests (chrome or firefox)')
